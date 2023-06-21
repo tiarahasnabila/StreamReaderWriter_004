88f00c0ceee308f6d2d19b139ef056ed82bd2a00
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-21 16:04:30 +0700

    membuka file kembali

--- pro2/pro2.cpp
+++ pro2/pro2.cpp
@@ -30,4 +30,9 @@ int main() {
 	// selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
+	//Membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt", ios::in);
+
 	
