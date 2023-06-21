69d588b1d014a53f46c4ae78789b6f5f9e434387
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-21 15:33:38 +0700

    membuat variabel

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -6,10 +6,4 @@ using namespace std;
 int main() {
 	string baris;
 
-	//membuka file dalam mode menulis.
-	ofstream outfile;
-	//menunjuk ke sebuah nama file
-	outfile.open("contohfile.txt");
-
-
-}
+	
