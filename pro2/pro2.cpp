98da82cb723d316b4e604787ec9d48e2ae2d6938
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-21 16:04:55 +0700

    membuka dan membaca file

--- pro2/pro2.cpp
+++ pro2/pro2.cpp
@@ -35,4 +35,17 @@ int main() {
 	//menunjuk ke sebuah file
 	infile.open(NamaFile + ".txt", ios::in);
 
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file anda maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
 	
