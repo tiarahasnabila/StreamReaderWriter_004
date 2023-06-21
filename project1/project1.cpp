ac4e0c73a636c26595492369894db9c4ac374e5c
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-21 15:58:35 +0700

    memasukkan kaata

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -31,4 +31,17 @@ int main() {
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
 
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file anda maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
 	
