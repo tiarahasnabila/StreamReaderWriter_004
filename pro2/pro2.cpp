fd75bde3097fbb8e96cff3b4b4ab68d9c7c2283a
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-21 16:02:09 +0700

    membuka file

--- pro2/pro2.cpp
+++ pro2/pro2.cpp
@@ -10,4 +10,11 @@ int main() {
 	cout << "Masukan Nama File : ";
 	cin >> NamaFile;
 
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
 	
